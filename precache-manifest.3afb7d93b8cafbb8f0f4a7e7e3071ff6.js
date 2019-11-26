a249a8a80d365cd6fdf799675ec0fc6bcef494f3
asafto <asafto@gmail.com>
2019-11-26 16:58:16 +0200

    Updates

---
+++ precache-manifest.3afb7d93b8cafbb8f0f4a7e7e3071ff6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc268ea3182116bca80b84c849839205",
+    "url": "%20http://asafto.github.io/app-instagram-ui/index.html"
+  },
+  {
+    "revision": "aa4a23b81acb1f465b2d",
+    "url": "%20http://asafto.github.io/app-instagram-ui/static/css/main.ab69818e.chunk.css"
+  },
+  {
+    "revision": "c06d7005410c7d8baa59",
+    "url": "%20http://asafto.github.io/app-instagram-ui/static/js/2.4d7e05b4.chunk.js"
+  },
+  {
+    "revision": "aa4a23b81acb1f465b2d",
+    "url": "%20http://asafto.github.io/app-instagram-ui/static/js/main.2dbaa584.chunk.js"
+  },
+  {
+    "revision": "c26d5250178bc2a15a32",
+    "url": "%20http://asafto.github.io/app-instagram-ui/static/js/runtime-main.cb38f079.js"
+  }
+]);
