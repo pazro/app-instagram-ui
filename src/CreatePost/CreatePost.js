1788d2058dc94e39aa3a13c3d64c5e9f82d92987
asafto <asafto@gmail.com>
2019-12-04 20:40:09 +0200

    additional corrections with formData

--- src/CreatePost/CreatePost.js
+++ src/CreatePost/CreatePost.js
@@ -5,6 +5,7 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import postModel from '../models/post.model';
 import {FaCloudUploadAlt, FaFileImage, FaRegLightbulb} from "react-icons/all";
 import TagsInput from 'react-tagsinput';
+import config from '../config';
 
 class CreatePost extends Component {
 
@@ -21,56 +22,69 @@ class CreatePost extends Component {
         })
     }
 
+    convertToFormData(values) {
+        const data = new FormData();
+        for (let key in values) {
+            Array.isArray(values[key])
+                ? values[key].forEach(value => data.append(key + '[]', value))
+                : data.append(key, values[key]) ;
+        }
+        return data;
+    }
+
     submit(values) {
-        console.log(values);
+        const formData = this.convertToFormData(values);
+        fetch(config.apiUrl + '/api/posts', {
+            method: 'POST',
+            body: formData
+        }).then(res => res.json())
+            .then(post => console.log(post))
+            .catch(err => console.log(err));
     }
 
     render() {
         return (
             <div className="create-post">
                 {/*<h2 className="form-title">Create Post:</h2>*/}
-                <Formik initialValues={{image: '', title: '', tags: ''}}
+                <Formik initialValues={{image: '', title: '', tags: []}}
                         validationSchema={postModel}
-                        onSubmit={this.submit.bind(this)}>
-                    <Form className="col-xs-12 col-sm-6">
-                        <h2 className="form-title">Create Post:</h2>
-                        <BForm.Group controlId="formBasicFile">
-                            <BForm.Label>Load Image:</BForm.Label>
-                            <Field type="file" name="image" id="image" className="form-control-image"/>
-                            <label htmlFor="image" className="upload-btn">
+                        onSubmit={this.submit.bind(this)}
+                        render={({setFieldValue}) => {
+                            return <Form className="col-xs-12 col-sm-6">
+                                <h2 className="form-title">Create Post:</h2>
+                                <BForm.Group controlId="formBasicFile">
+                                    <BForm.Label>Load Image:</BForm.Label>
+                                    <input type="file" name="image" id="image" className="form-control-image" onChange={(event => {
+                                        setFieldValue('image', event.currentTarget.files[0]);
+                                    })}/>
+                                    <label htmlFor="image" className="upload-btn">
                                 <span>
                                     <FaFileImage className="upload-icon"/>
                                     Select your image...
                                 </span>
-                            </label>
-                            {/*<br/><Button variant="primary" className="upload-btn">*/}
-                            {/*    <FaFileImage className="upload-icon"/>*/}
-                            {/*    Select your image...*/}
-                            {/*</Button>*/}
-                        </BForm.Group>
-                        <BForm.Group controlId="formBasicEmail">
-                            <BForm.Label>Post Title:</BForm.Label>
-                            <Field name="title" className="form-control" placeholder="Place your post title here"/>
-                            <ErrorMessage className="alert alert-danger" name="title" component="div" />
-                        </BForm.Group>
-                        <BForm.Group controlId="formBasicEmail">
-                            <BForm.Label>Post Tags:</BForm.Label>
-                            {/*<Field*/}
-                            {/*    name="tags"*/}
-                            {/*    className="form-control"*/}
-                                <TagsInput
-                                value={this.state.tags}
-                                onChange={this.handleTagsChange.bind(this)}
-                                className="tags-input"
-                                />
-                            <FaRegLightbulb/><span>Press Enter for each tag</span>
-                            {/*/>*/}
-                        </BForm.Group>
-                        <Button variant="primary" type="submit" className="share-btn">
-                            <FaCloudUploadAlt className="share-icon" />
-                            Upload your post...
-                        </Button>
-                    </Form>
+                                    </label>
+                                </BForm.Group>
+                                <BForm.Group controlId="formBasicEmail">
+                                    <BForm.Label>Post Title:</BForm.Label>
+                                    <Field name="title" className="form-control" placeholder="Place your post title here"/>
+                                    <ErrorMessage className="alert alert-danger" name="title" component="div" />
+                                </BForm.Group>
+                                <BForm.Group controlId="formBasicEmail">
+                                    <BForm.Label>Post Tags:</BForm.Label>
+                                    <TagsInput
+                                        value={this.state.tags}
+                                        onChange={this.handleTagsChange.bind(this)}
+                                        className="tags-input"
+                                    />
+                                    <FaRegLightbulb/><span>Press Enter for each tag</span>
+                                    {/*/>*/}
+                                </BForm.Group>
+                                <Button variant="primary" type="submit" className="share-btn">
+                                    <FaCloudUploadAlt className="share-icon" />
+                                    Upload your post...
+                                </Button>
+                            </Form>
+                    }}  >
                 </Formik>
             </div>
 
--- src/models/post.model.js
+++ src/models/post.model.js
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 let schema = yup.object().shape({
     title: yup.string().min(2).max(255).required(),
     image: yup.mixed().required(),
-    tags: yup.string()
+    tags: yup.array()
 });
 
 export default schema;
